01bbc765e6a2eca329fccb072fde551246dc70ed
Ardee <goobama@outlook.com>
2016-05-17 03:51:28 +0800

    完善个人信息功能
    可以修改个人信息，以及添加所教班级，但是有bug。

--- AppraisalSystem/Colleague/ColleagueController/MineInforViewController.m
+++ AppraisalSystem/Colleague/ColleagueController/MineInforViewController.m
@@ -7,18 +7,144 @@
 //
 
 #import "MineInforViewController.h"
+#import "LTPickerView.h"
+#import <BmobSDK/Bmob.h>
 
 @interface MineInforViewController ()
 
+{
+    NSMutableArray *_ourClassData;
+    UIBarButtonItem *_saveBtn;
+}
 @end
 
 @implementation MineInforViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self ourClass];
+    [self ourButton];
 }
-
+#pragma mark ----为按钮添加点击事件--
+- (void)ourButton
+{
+    [_mineSex addTarget:self action:@selector(chanceSex) forControlEvents:UIControlEventTouchUpInside];
+    [_mineClass1 addTarget:self action:@selector(chanceClass:) forControlEvents:UIControlEventTouchUpInside];
+    [_mineClass2 addTarget:self action:@selector(chanceClass:) forControlEvents:UIControlEventTouchUpInside];
+    [_mineClass3 addTarget:self action:@selector(chanceClass:) forControlEvents:UIControlEventTouchUpInside];
+    _saveBtn = [[UIBarButtonItem alloc]initWithTitle:@"保存" style:UIBarButtonItemStylePlain target:self action:@selector(saveMineInformation)];
+    self.navigationItem.rightBarButtonItem = _saveBtn;
+    
+}
+#pragma mark ----创建有多少班级的数据源--
+- (void)ourClass
+{
+    _ourClassData = [[NSMutableArray alloc]initWithObjects:@"高一1班",@"高一2班",@"高一3班",@"高一4班",@"高一5班",@"高一6班",@"高一7班",@"高一8班",@"高一9班",@"高一10班",@"高二1班",@"高二2班",@"高二3班",@"高二4班",@"高二5班",@"高二6班",@"高二7班",@"高二8班",@"高二9班",@"高二10班",@"高三1班",@"高三2班",@"高三3班",@"高三4班",@"高三5班",@"高三6班",@"高三7班",@"高三8班",@"高三9班",@"高三10班", nil];
+//    BmobQuery *query = [[BmobQuery alloc] init];
+//    NSString *sql = [NSString stringWithFormat:@"select * from UserTable where UserName = '%@'",_mineName.text];
+//    [query queryInBackgroundWithBQL:sql block:^(BQLQueryResult *result, NSError *error)
+//     {
+//         if (error)
+//         {
+//             NSLog(@"error=%@",error);
+//         }
+//         else if(result)
+//         {
+//             NSArray *resultArr = [NSArray arrayWithArray:result.resultsAry];
+//             for (BmobObject *bObject in resultArr)
+//             {
+//                 _bmobOjt = bObject;
+//             }
+//         }
+//     }];
+}
+#pragma mark ----保存按钮的点击事件--
+- (void)saveMineInformation
+{
+    [_mineObject setObject:_mineName.text forKey:@"UserName"];
+    [_mineObject setObject:_mineSex.titleLabel.text forKey:@"UserSex"];
+    [_mineObject setObject:_mineTel.text forKey:@"UserTel"];
+    [_mineObject updateInBackgroundWithResultBlock:^(BOOL isSuccessful, NSError *error)
+    {
+        if (error)
+        {
+            NSLog(@"error=======%@",error);
+        } else if (isSuccessful)
+        {
+            NSLog(@"更新成功！！");
+        }
+    }];
+    if (![_mineClass1.titleLabel.text isEqualToString:@"添加班级"])
+    {
+        BmobObject *bObj = [BmobObject objectWithClassName:@"UserClass"];
+        [bObj setObject:[_mineObject objectForKey:@"UserName"] forKey:@"UserName"];
+        [bObj setObject:_mineClass1.titleLabel.text forKey:@"Class1"];
+        if (![_mineClass2.titleLabel.text isEqualToString:@"添加班级"])
+        {
+            [bObj setObject:_mineClass2.titleLabel.text forKey:@"Class2"];
+            if (![_mineClass3.titleLabel.text isEqualToString:@"添加班级"])
+            {
+                [bObj setObject:_mineClass3.titleLabel.text forKey:@"Class3"];
+                
+            }
+        }
+        [bObj saveInBackgroundWithResultBlock:^(BOOL isSuccessful, NSError *error)
+        {
+            if (error)
+            {
+                NSLog(@"error===%@",error);
+            } else if (isSuccessful)
+            {
+                NSLog(@"保存班级成功！！");
+            }
+        }];
+    }
+}
+#pragma mark ----选择性别的点击事件--
+- (void)chanceSex
+{
+    LTPickerView* pickerView = [LTPickerView new];
+    pickerView.dataSource = @[@"男",@"女"];//设置要显示的数据
+    pickerView.defaultStr = @"男";//默认选择的数据
+    [pickerView show];//显示
+    //回调block
+    pickerView.block = ^(LTPickerView* obj,NSString* str,int num){
+        //obj:LTPickerView对象
+        //str:选中的字符串
+        //num:选中了第几行
+        NSLog(@"选择了第%d行的%@",num,str);
+        [_mineSex setTitle:str forState:UIControlStateNormal];
+    };
+}
+#pragma mark ----选择班级的点击事件--
+- (void)chanceClass:(UIButton *)classBtn
+{
+    LTPickerView* pickerView = [LTPickerView new];
+    pickerView.dataSource = _ourClassData;//设置要显示的数据
+    pickerView.defaultStr = @"高二5班";//默认选择的数据
+    [pickerView show];//显示
+    //回调block
+    pickerView.block = ^(LTPickerView* obj,NSString* str,int num){
+        //obj:LTPickerView对象
+        //str:选中的字符串
+        //num:选中了第几行
+        NSLog(@"选择了第%d行的%@",num,str);
+        if (classBtn.tag==1)
+        {
+            [_mineClass1 setTitle:str forState:UIControlStateNormal];
+            
+        }
+        else if (classBtn.tag==2)
+        {
+            [_mineClass2 setTitle:str forState:UIControlStateNormal];
+        }
+        else
+        {
+            [_mineClass3 setTitle:str forState:UIControlStateNormal];
+        }
+        [_ourClassData removeObject:str];
+    };
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -28,6 +154,10 @@
 {
     _mineName.text = [_mineObject objectForKey:@"UserName"];
     [_mineSex setTitle:[_mineObject objectForKey:@"UserSex"] forState:UIControlStateNormal];
+    if(![_mineObject objectForKey:@"UserSex"])
+    {
+        [_mineSex setTitle:@"请选择" forState:UIControlStateNormal];
+    }
     _mineTel.text = [_mineObject objectForKey:@"UserTel"];
     
 }
